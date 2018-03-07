a3bafae7f9598af1f7453bf9377e549e879b69d1
Gianni Azizi <gianni.azizi@bnpparibas-pf.com>
2018-03-07 16:25:43 +0100

    Change configuration for mongodb

--- src/main/java/fr/esgi/config/MongoConfiguration.java
+++ src/main/java/fr/esgi/config/MongoConfiguration.java
@@ -1,8 +1,8 @@
 package fr.esgi.config;
 
 import com.github.mongobee.Mongobee;
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
+import com.mongodb.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
@@ -11,15 +11,46 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.util.Collections;
 
 /**
  * Created by gianniazizi on 06/03/2018.
  */
 @Configuration
-@Import(MongoAutoConfiguration.class)
+@Import(value = MongoAutoConfiguration.class)
 @EnableMongoRepositories({"fr.esgi.domain.repositories"})
-public class MongoConfiguration {
+public class MongoConfiguration extends AbstractMongoConfiguration{
+
+    @Autowired
+    private MongoClient mongoClient;
+
+    @Autowired
+    private MongoProperties mongoProperties;
+
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return mongoProperties.getDatabase();
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        return mongoClient;
+    }
+
     @Bean
     public Mongobee mongobee(MongoClient mongoClient, MongoTemplate mongoTemplate, MongoProperties mongoProperties) {
         Mongobee mongobee = new Mongobee(mongoClient);
